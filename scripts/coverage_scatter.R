05f4f27c22254ece4e79b7164eeedd3de1df5a09
Dave Sims <misc@lamneth.net>
2014-03-09 17:11:26 -0400

    Added an output dir argument to put the graphs into the correct directory

--- scripts/coverage_scatter.R
+++ scripts/coverage_scatter.R
@@ -5,14 +5,15 @@ getwd()
 
 # Get some commandline args
 args <- commandArgs( trailingOnly = TRUE )
-if( length( args ) !=2 ) stop( "Script requires 2 arguments: sample_name and minimum_coverage_threshold." )
+if( length( args ) !=3 ) stop( "Script requires 3 arguments: sample_name, minimum_coverage_threshold, and output dir." )
 sample <- args[1]
 threshold <- args[2]
+outdir <- args[3]
 
 # Make sure we have ggplot2 installed
 if(!require(ggplot2)) stop( "Need to install ggplot2" )
 
-file <- "AllAmpliconsCoverage.tsv"
+file <- paste0( outdir, "/AllAmpliconsCoverage.tsv" )
 coverage_data <- read.table( file = file, sep = "\t", header = TRUE ) 
 
 scatter <- ggplot( coverage_data, aes( x = Length, y = Median ) ) +
@@ -23,5 +24,5 @@ scatter <- ggplot( coverage_data, aes( x = Length, y = Median ) ) +
            ylab( "Median Coverage for Amplicon" ) +
            ggtitle( paste0( "Coverage Versus Amplicon Length Plot for ", sample ) )
 
-ggsave( filename = "Amp_Coverage_vs_Length_Plot.png", plot = scatter )
-ggsave( filename = "Amp_Coverage_vs_Length_Plot.pdf", plot = scatter )
+ggsave( filename = paste0( outdir, "/Amp_Coverage_vs_Length_Plot.png" ), plot = scatter )
+ggsave( filename = paste0( outdir, "/Amp_Coverage_vs_Length_Plot.pdf" ), plot = scatter )
